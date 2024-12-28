47e64da6073cac5e1c339ad1081c9dc5a6236069
Akramerabti <akramerabti@gmail.com>
2024-12-27 23:32:36 -0500

    frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -23,5 +23,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
       {import.meta.env.DEV && <ReactQueryDevtools />}
     </QueryClientProvider>
-  </React.StrictMode>
+   </React.StrictMode>
 );
