eb1f449c9c50b9349a829d671be11ebbb162ac59
Akramerabti <akramerabti@gmail.com>
2025-09-26 08:08:44 -0400

    s

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1053,13 +1053,17 @@ const Profile = () => {
 
         {/* Crop Modal */}
         {cropModalProps && (
-          <ImageCropModal
-            image={cropModalProps.image}
-            onCropComplete={cropModalProps.onCropComplete}
-            onClose={handleCloseCropModal}
-            aspectRatio={1}
-          />
-        )}
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div className="w-full max-w-lg max-h-[90vh] overflow-hidden">
+      <ImageCropModal
+        image={cropModalProps.image}
+        onCropComplete={cropModalProps.onCropComplete}
+        onClose={handleCloseCropModal}
+        aspectRatio={1}
+      />
+    </div>
+  </div>
+)}
 
         {/* Location Request Modal */}
         <LocationRequestModal
